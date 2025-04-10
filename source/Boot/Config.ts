0212b1259451836a7ce8a84a10540d18c9a8943f
gestordeveloper <109088147+gestordeveloper@users.noreply.github.com>
2025-04-10 20:31:36 -0300

    [SSK] Mudando url da api

--- source/Boot/Config.ts
+++ source/Boot/Config.ts
@@ -5,4 +5,4 @@ export const CONF_PAYXE_API_URL = "https://payxe.site/api";
 export const CONF_PAYXE_API_EMAIL = "contato@agenciaebenezer.com.br";
 export const CONF_PAYXE_API_PASSWORD = "@88325741Saa";
 
-export const CONF_VAPZAP_API_URL = "https://api.vapzap.com.br";
+export const CONF_VAPZAP_API_URL = "https://apisakamoto.agenciaebenezer.com.br";
